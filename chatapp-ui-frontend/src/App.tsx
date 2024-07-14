511d7bf14a4702788cc7f90782abce10b213b7a8
kowshik <kowshikprogrammer@gmail.com>
2024-07-15 01:30:47 +0530

    configured

---
+++ chatapp-ui-frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <div>hello world</div>
+      <Button>kowshik</Button>
+    </>
+  );
+}
+
+export default App;
